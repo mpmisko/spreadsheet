e2d14940e5a886dd5397fe1eaa833492da657bee
mp5217 <michal.pandy17@imperial.ac.uk>
2018-02-26 15:00:03 +0000

    Implementation of simple spreadsheet and main method

--- src/spreadsheet/Main.java
+++ src/spreadsheet/Main.java
@@ -1,11 +1,21 @@
 package spreadsheet;
 
+import common.gui.SpreadsheetGUI;
+
 public class Main {
 
   private static final int DEFAULT_NUM_ROWS = 5000;
   private static final int DEFAULT_NUM_COLUMNS = 5000;
 
   public static void main(String[] args) {
+    Spreadsheet s = new Spreadsheet();
+    SpreadsheetGUI gui = new SpreadsheetGUI(s, DEFAULT_NUM_ROWS, DEFAULT_NUM_COLUMNS);
+    if(args.length > 0) {
+      int rows = Integer.parseInt(args[0]);
+      int columns = Integer.parseInt(args[1]);
+      gui = new SpreadsheetGUI(s, rows, columns);
+    }
+    gui.start();
   }
 
 }
--- src/spreadsheet/Spreadsheet.java
+++ src/spreadsheet/Spreadsheet.java
@@ -1,6 +1,28 @@
 package spreadsheet;
 
+import common.api.CellLocation;
 import common.api.Tabular;
+import common.api.value.Value;
 
 public class Spreadsheet implements Tabular {
+
+  @Override
+  public void setExpression(CellLocation location, String expression) {
+
+  }
+
+  @Override
+  public String getExpression(CellLocation location) {
+    return null;
+  }
+
+  @Override
+  public Value getValue(CellLocation location) {
+    return null;
+  }
+
+  @Override
+  public void recompute() {
+
+  }
 }
